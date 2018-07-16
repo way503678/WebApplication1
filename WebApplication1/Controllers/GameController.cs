4937ad0249b089898e9dfee8fc4d4ce7df4878fb
johnson <way503678@hotmail.com>
2018-07-16 14:54:50 +0800

    分開更改

--- WebApplication1/Controllers/GameController.cs
+++ WebApplication1/Controllers/GameController.cs
@@ -36,30 +36,7 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        public ActionResult Login()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult AuthorRize(WebApplication1.Models.Menber member)
-        {
-            using (Models.masterEntities db = new Models.masterEntities())
-            {
-                var user = db.Menber.Where(x => x.Account == member.Account && x.passwd == member.passwd).FirstOrDefault();
-                if (user == null)
-                {
-
-                    member.LoginErrorMessage = "帳號或密碼錯誤";
-                    return View("Login", member);
-                }
-                else
-                {
-                    Session["userid"] = member.Account;
-                    return RedirectToAction("Home", "Game");
-                }
-            }
-        }
+        
 
         
     }
---
+++ WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AuthorRize(WebApplication1.Models.Menber member)
+        {
+            using (Models.masterEntities db = new Models.masterEntities())
+            {
+                var user = db.Menber.Where(x => x.Account == member.Account && x.passwd == member.passwd).FirstOrDefault();
+                if (user == null)
+                {
+
+                    member.LoginErrorMessage = "帳號或密碼錯誤";
+                    return View("Login", member);
+                }
+                else
+                {
+                    Session["userid"] = member.Account;
+                    return RedirectToAction("Home", "Game");
+                }
+            }
+        }
+    }
+}
