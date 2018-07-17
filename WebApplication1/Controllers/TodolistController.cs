35a17ffd4ce098a7e8874af60be635c865282685
johnson <way503678@hotmail.com>
2018-07-17 15:28:13 +0800

    完成代辦事項

--- WebApplication1/Controllers/TodolistController.cs
+++ WebApplication1/Controllers/TodolistController.cs
@@ -18,12 +18,42 @@ namespace WebApplication1.Controllers
             {
                 var dolist = db.todolist.SqlQuery("select * from todolist where userid = '" + userid + "'").ToList();
                 ViewBag.todolist = dolist;
-                ViewBag.count = dolist.Count();
                 return View();
 
 
             }
 
         }
+        [HttpPost]
+        public ActionResult Deletestaff(WebApplication1.Models.todolist todolist,string button)
+        {            
+            var userid = Session["userid"].ToString();
+            using (Models.masterEntities1 db = new Models.masterEntities1())
+            {
+                var update = db.todolist.Where(x => x.staffid.ToString() == button).FirstOrDefault();
+                update.statu = "1";
+                db.SaveChanges();               
+                return RedirectToAction("Todolist");
+            }
+         }
+
+        [HttpPost]
+        public ActionResult Addnewstaff(WebApplication1.Models.todolist todolist, string addnewstaff)
+        {
+            Models.todolist todo = new Models.todolist
+            {
+                userid = Session["userid"].ToString(),
+                staff = addnewstaff,
+                statu = "0"
+            };
+            using (Models.masterEntities1 db = new Models.masterEntities1())
+            {               
+                db.todolist.Add(todo);
+                db.SaveChanges();                
+            }
+            return RedirectToAction("Todolist");
+        }
+
+
     }
 }
